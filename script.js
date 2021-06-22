b1052c71b7e4a4731e188ddde61dcbd5bac06023
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-22 14:34:51 +0200

    retrait jquery

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-$('.image').click(function () {
-    $('.image').removeClass('active');
-    $(this).addClass('active');
-});
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const clearActive = () => {
+    $$('.image').forEach(image => {
+            image.classList.remove('active');
+    })
+}
+
+$$('.image').forEach(image => {
+    image.addEventListener('click', () => {
+        clearActive();
+        image.classList.add('active');
+    })
+})
+
